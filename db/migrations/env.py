063214819653bfbdf7a0fe2ad7af70e526c6240a
kirbylockard <kirbylockard@gmail.com>
2025-09-23 20:31:55 -0500

    setup alembic migrations and created habit and habit table

---
+++ db/migrations/env.py
@@ -0,0 +1,38 @@
+from app.models.habit import Habit  # import models
+from alembic import context
+from sqlalchemy import engine_from_config, pool
+from app.core.database import Base, DATABASE_URL
+
+# Convert async URL to sync for Alembic
+sync_database_url = DATABASE_URL.replace("+asyncpg", "")
+
+config = context.config
+config.set_main_option("sqlalchemy.url", sync_database_url)
+
+target_metadata = Base.metadata
+
+
+def run_migrations_offline():
+    url = config.get_main_option("sqlalchemy.url")
+    context.configure(
+        url=url, target_metadata=target_metadata, literal_binds=True)
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def run_migrations_online():
+    connectable = engine_from_config(
+        config.get_section(config.config_ini_section),
+        prefix="sqlalchemy.",
+        poolclass=pool.NullPool,
+    )
+    with connectable.connect() as connection:
+        context.configure(connection=connection, target_metadata=target_metadata)
+        with context.begin_transaction():
+            context.run_migrations()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()

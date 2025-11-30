d29c12dd3e8668bda8839896b0cbd704d621fdb6
kirbylockard <kirbylockard@gmail.com>
2025-11-30 17:44:40 -0600

    added cors for connection to FE

--- app/main.py
+++ app/main.py
@@ -1,9 +1,22 @@
 # app/main.py
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from app.api import habits
 
 app = FastAPI(title="Lattivo API")
+
+origins = [
+    "http://localhost:3000",
+    "https://lattivo-frontend.vercel.app",  # replace with lattivo.com when rehosted
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(habits.router, prefix="/habits", tags=["habits"])
 
